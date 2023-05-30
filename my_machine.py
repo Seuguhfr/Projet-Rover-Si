952e87c11c2551692d4650b69079b465fc286b48
Seuguhfr <hdbdt1597@gmail.com>
2023-05-30 15:40:36 +0200

    Ajout du module 'my_machine' pour une execution local du programme (sans carte raspberry pi)

---
+++ my_machine.py
@@ -0,0 +1,15 @@
+class PWM():
+    def __init__(self, pin):
+        pass
+    
+    def duty_u16(self, vitesse):
+        pass
+
+class Pin():
+    def __init__(self, direc, mode=- 1):
+        pass
+    
+    def value(self, direc):
+        pass
+    
+    OUT = 1
