edb925663b3f13e0bb568556c593b042915b524c
Seuguhfr <hdbdt1597@gmail.com>
2023-06-06 08:48:37 +0200

    Transformation de Rover en sous classe de list

--- roverr.py
+++ roverr.py
@@ -2,8 +2,9 @@ from machine import Pin, PWM, reset_cause
 from time import sleep, time as actual_time
 from math import sin, cos, radians, pi
 
-class Rover:
+class Rover(list):
     def __init__(self, *moteurs):
+        super().__init__(moteurs)
         self.moteurs = list(moteurs)
         self.methodes = {
             'allez-retour': self.go_and_back,
